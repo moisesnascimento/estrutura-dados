f574a5670de96a657f7319a373711480e0c1bfaf
Moises Nascimento <moises.smith759@gmail.com>
2020-05-02 18:29:20 -0300

    att

--- src/main/java/com/github/moisesnascimento/estruturasdados/listas/ListaDuplamenteLigada.java
+++ src/main/java/com/github/moisesnascimento/estruturasdados/listas/ListaDuplamenteLigada.java
@@ -1,67 +1,162 @@
 package com.github.moisesnascimento.estruturasdados.listas;
 
-import com.github.moisesnascimento.estruturasdados.listas.ListaLigada.No;
-
 public class ListaDuplamenteLigada<T> implements Lista<T> {
+
+	private No<T> primeiro;
+	private int tamanho = 0;
 
 	@Override
 	public T recuperar(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= getTamanho()) {
+			throw new IndexOutOfBoundsException(index);
+		}
+		No<T> atual = primeiro;
+		while (index > 0) {
+			atual = atual.proximo;
+			index--;
+		}
+		return atual.valor;
 	}
 
 	@Override
 	public void trocar(int index, T valor) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void adicionar(T valor) {
-		// TODO Auto-generated method stub
-
+		if (primeiro == null) {
+			No<T> no = new No<>();
+			no.valor = valor;
+			primeiro = no;
+		} else {
+			No<T> atual = primeiro;
+			while (atual.proximo != null) {
+				atual = atual.proximo;
+			}
+			No<T> no = new No<>();
+			no.valor = valor;
+			atual.proximo = no;
+			no.anterior = atual;
+		}
+		tamanho++;
 	}
 
 	@Override
 	public void remover(T valor) {
-		// TODO Auto-generated method stub
-
+		if (estaVazia()) {
+			return;
+		}
+		No<T> atual = primeiro;
+		while (atual != null && !atual.valor.equals(valor)) {
+			atual = atual.proximo;
+		}
+		if (atual == null) {
+			return;
+		}
+		if (atual.proximo == null && atual.anterior == null) {
+			// remover se tiver um elemento
+			primeiro = null;
+		} else if (atual.proximo == null) {
+			// remover no final
+			atual.anterior.proximo = null;
+		} else if (atual.anterior == null) {
+			// remover no inicio
+			primeiro = atual.proximo;
+			atual.anterior = null;
+		} else {
+			// remover no meio
+			atual.anterior.proximo = atual.proximo;
+			atual.proximo.anterior = atual.anterior;
+		}
+		tamanho--;
 	}
 
 	@Override
 	public void remover(int index) {
-		// TODO Auto-generated method stub
-
+		if (index < 0 || index >= getTamanho()) {
+			throw new IndexOutOfBoundsException(index);
+		}
+		No<T> atual = primeiro;
+		while (index > 0) {
+			atual = atual.proximo;
+			index--;
+		}
+		if (atual.proximo == null && atual.anterior == null) {
+			primeiro = null;
+		} else if (atual.proximo == null) {
+			atual.anterior.proximo = null;
+		} else if (atual.anterior == null) {
+			primeiro = atual.proximo;
+			atual.anterior = null;
+		} else {
+			atual.anterior.proximo = atual.proximo;
+			atual.proximo.anterior = atual.anterior;
+		}
+		tamanho--;
 	}
 
 	@Override
 	public boolean contem(T valor) {
-		// TODO Auto-generated method stub
-		return false;
+		No<T> atual = primeiro;
+		while (atual != null && !atual.valor.equals(valor)) {
+			atual = atual.proximo;
+		}
+		return atual != null;
 	}
 
 	@Override
 	public int recuperarIndex(T valor) {
-		// TODO Auto-generated method stub
-		return 0;
+		int indexAtual = 0;
+		No<T> atual = primeiro;
+		while (atual != null && !atual.valor.equals(valor)) {
+			atual = atual.proximo;
+			indexAtual++;
+		}
+		if (atual == null) {
+			return -1;
+		}
+		return indexAtual;
 	}
 
 	@Override
 	public int getTamanho() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.tamanho;
 	}
 
 	@Override
 	public void adicionar(int index, T valor) {
-		// TODO Auto-generated method stub
-
+		if (index < 0 || index > getTamanho()) {
+			throw new IndexOutOfBoundsException(index);
+		}
+		No<T> atual = primeiro;
+		No<T> anterior = null;
+		while (index > 0) {
+			anterior = atual;
+			atual = atual.proximo;
+			index--;
+		}
+		No<T> no = new No<>();
+		no.valor = valor;
+		if (atual == null) {
+			anterior.proximo = no;
+			no.anterior = anterior;
+		} else if (atual.anterior == null) {
+			primeiro = no;
+			no.proximo = atual;
+			atual.proximo = no;
+		} else {
+			atual.anterior.proximo = no;
+			no.anterior = atual.anterior;
+			no.proximo = atual;
+			atual.anterior = no;
+		}
+		tamanho++;
 	}
 
 	@Override
 	public boolean estaVazia() {
-		// TODO Auto-generated method stub
-		return false;
+		return tamanho == 0;
 	}
 
 	private class No<E> {
---
+++ src/test/java/com/github/moisesnascimento/estruturasdados/listas/ListaDuplamenteLiagadaTeste.java
@@ -0,0 +1,33 @@
+package com.github.moisesnascimento.estruturasdados.listas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ListaDuplamenteLiagadaTeste {
+
+	@Test
+	public void adicionarNaPosicaoEspecificaDeveAdicionar() {
+		ListaDuplamenteLigada<String> lista = new ListaDuplamenteLigada<>();
+		lista.adicionar("teste1");
+		lista.adicionar("teste2");
+		lista.adicionar("teste3");
+		lista.adicionar(1, "teste4");
+		assertEquals("teste1", lista.recuperar(0));
+		assertEquals("teste4", lista.recuperar(1));
+		assertEquals("teste2", lista.recuperar(2));
+		assertEquals("teste3", lista.recuperar(3));
+	}
+	@Test
+	public void adicionarNaPosicaoIncialEspecificaDeveAdicionar() {
+		ListaDuplamenteLigada<String> lista = new ListaDuplamenteLigada<>();
+		lista.adicionar("teste1");
+		lista.adicionar("teste2");
+		lista.adicionar("teste3");
+		lista.adicionar(0, "teste4");
+		assertEquals("teste4", lista.recuperar(0));
+		assertEquals("teste1", lista.recuperar(1));
+		assertEquals("teste2", lista.recuperar(2));
+		assertEquals("teste3", lista.recuperar(3));
+	}
+}
